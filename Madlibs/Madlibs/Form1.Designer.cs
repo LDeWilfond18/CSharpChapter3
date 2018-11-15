d026d09700b429b67bbae037bb772735e1663967
Logan <logan.dewilfond@gmail.com>
2018-11-15 12:50:32 -0600

    update

---
+++ Madlibs/Madlibs/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Madlibs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtEst = new System.Windows.Forms.TextBox();
+            this.txtBodyPart = new System.Windows.Forms.TextBox();
+            this.txtAnimal = new System.Windows.Forms.TextBox();
+            this.txtNoun = new System.Windows.Forms.TextBox();
+            this.txtNounPlural = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(53, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(53, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Word ending est";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(53, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Body Parts Plural";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(53, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Animal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(52, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Noun";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(52, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Noun Plural";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(52, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Enter numba";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label8.Location = new System.Drawing.Point(52, 355);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(155, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Enter anotha numba";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(56, 42);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 8;
+            // 
+            // txtEst
+            // 
+            this.txtEst.Location = new System.Drawing.Point(56, 89);
+            this.txtEst.Name = "txtEst";
+            this.txtEst.Size = new System.Drawing.Size(100, 20);
+            this.txtEst.TabIndex = 9;
+            // 
+            // txtBodyPart
+            // 
+            this.txtBodyPart.Location = new System.Drawing.Point(56, 138);
+            this.txtBodyPart.Name = "txtBodyPart";
+            this.txtBodyPart.Size = new System.Drawing.Size(100, 20);
+            this.txtBodyPart.TabIndex = 10;
+            // 
+            // txtAnimal
+            // 
+            this.txtAnimal.Location = new System.Drawing.Point(57, 189);
+            this.txtAnimal.Name = "txtAnimal";
+            this.txtAnimal.Size = new System.Drawing.Size(100, 20);
+            this.txtAnimal.TabIndex = 11;
+            // 
+            // txtNoun
+            // 
+            this.txtNoun.Location = new System.Drawing.Point(57, 238);
+            this.txtNoun.Name = "txtNoun";
+            this.txtNoun.Size = new System.Drawing.Size(100, 20);
+            this.txtNoun.TabIndex = 12;
+            // 
+            // txtNounPlural
+            // 
+            this.txtNounPlural.Location = new System.Drawing.Point(57, 289);
+            this.txtNounPlural.Name = "txtNounPlural";
+            this.txtNounPlural.Size = new System.Drawing.Size(100, 20);
+            this.txtNounPlural.TabIndex = 13;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(56, 333);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 14;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(57, 379);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(263, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(463, 46);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "do it";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblOutput.Location = new System.Drawing.Point(263, 138);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(463, 261);
+            this.lblOutput.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.txtNounPlural);
+            this.Controls.Add(this.txtNoun);
+            this.Controls.Add(this.txtAnimal);
+            this.Controls.Add(this.txtBodyPart);
+            this.Controls.Add(this.txtEst);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtEst;
+        private System.Windows.Forms.TextBox txtBodyPart;
+        private System.Windows.Forms.TextBox txtAnimal;
+        private System.Windows.Forms.TextBox txtNoun;
+        private System.Windows.Forms.TextBox txtNounPlural;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Madlibs/Madlibs/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Madlibs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string color;
+            string wordEndEst;
+            string animal;
+            string bodyPartPlural;
+            string noun;
+            string nounPlural;
+            int num1;
+            int num2;
+            int num3;
+
+            color = txtColor.Text;
+            wordEndEst = txtEst.Text;
+            animal = txtAnimal.Text;
+            bodyPartPlural = txtBodyPart.Text;
+            noun = txtNoun.Text;
+            nounPlural = txtNounPlural.Text;
+            num1 = Convert.ToInt32(txtNum1);
+            num2 = Convert.ToInt32(txtNum2);
+
+            num3 = num1 + num2;
+
+            lblOutput.Text = "The " + color + " Dragon is the " + wordEndEst + " Dragon of all.It has " + num3 + " " + bodyPartPlural + " and a " + animal + " shaped like a " + noun + ". It loves to eat(plural noun), although it will feast on nearly anything."
+        }
+    }
+}
